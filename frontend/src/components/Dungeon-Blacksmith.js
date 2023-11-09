d31e9ed31bbbf37f10924908c73414a60125db6d
chattrapan <copasstragoon@gmail.com>
2023-11-09 11:46:02 +0700

    Add BS Claymore SSR9

--- frontend/src/components/Dungeon-Blacksmith.js
+++ frontend/src/components/Dungeon-Blacksmith.js
@@ -443,7 +443,7 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                 functionName: 'allowance',
                 args: [address, enchantR],
             })
-            if (cmjAllow < (750 * 10**18)) {
+            if (cmjAllow < (800 * 10**18)) {
                 const config = await prepareWriteContract({
                     address: cmjToken,
                     abi: erc20ABI,
@@ -459,7 +459,7 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                 functionName: 'allowance',
                 args: [address, enchantR],
             })
-            if (jdaoAllow < (55 * 10**18)) {
+            if (jdaoAllow < (60 * 10**18)) {
                 const config2 = await prepareWriteContract({
                     address: jdaoToken,
                     abi: erc20ABI,
@@ -475,7 +475,7 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                 functionName: 'allowance',
                 args: [address, enchantR],
             })
-            if (osAllow < (2770 * 10**18)) {
+            if (osAllow < (3850 * 10**18)) {
                 const config3 = await prepareWriteContract({
                     address: osToken,
                     abi: erc20ABI,
@@ -1352,7 +1352,7 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                                             ██████╦╝██████╔╝  ╚█████╔╝███████╗██║░░██║░░░██║░░░██║░╚═╝░██║╚█████╔╝██║░░██║███████╗
                                             ╚═════╝░╚═════╝░  ░╚════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝
                                             */}
-                                            {String(item.Id).slice(0, 3) === "710" && Number(item.Id) % 100000 !== 4250 && Number(item.Id) % 100000 !== 14800 && Number(item.Id) % 100000 !== 18050 ?
+                                            {String(item.Id).slice(0, 3) === "710" && Number(item.Id) % 100000 !== 4250 && Number(item.Id) % 100000 !== 14800 && Number(item.Id) % 100000 !== 25700 ?
                                                 <div style={{justifyContent: "space-around", padding: "30px", marginRight: "50px"}} className="nftCard">
                                                     <div className="emp pixel" style={{marginTop: "10px", width: "350px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
                                                         <div>
@@ -1491,6 +1491,12 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                                                             <div>
                                                                 <img src="https://bafybeifeshgxjiddolfuttdsgspbzdp7y7zjws4eoctklyi2wzjlbg74eu.ipfs.nftstorage.link" width="120" alt="Can not load metadata." />
                                                                 <div style={{width: "150px"}}>{item.Name.slice(0, -1)}8</div>
+                                                            </div>
+                                                        }
+                                                        {Number(item.Id) % 100000 === 18050 &&
+                                                            <div>
+                                                                <img src="https://bafybeibfhaxkxp5z634v6rvhcymrcl2mtfl7keiecmbsgg5ygi7ofv2xsa.ipfs.nftstorage.link" width="120" alt="Can not load metadata." />
+                                                                <div style={{width: "150px"}}>{item.Name.slice(0, -1)}9</div>
                                                             </div>
                                                         }
                                                     </div>
@@ -1811,6 +1817,18 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                                                                 </div>
                                                             </>
                                                         }
+                                                        {Number(item.Id) % 100000 === 18050 &&
+                                                            <>
+                                                                <div>
+                                                                    <div>Level 8</div>
+                                                                    <div style={{width: "150px"}}>{item.RewardPerSec} cmpow per sec</div>
+                                                                </div>
+                                                                <div>
+                                                                    <div>Level 9</div>
+                                                                    <div style={{width: "150px"}}>{item.RewardPerSec + 7650} cmpow per sec</div>
+                                                                </div>
+                                                            </>
+                                                        }
                                                     </div>
                                                     <div style={{width: "100%", borderBottom: "1px solid #dddade", marginTop: "10px"}}></div>
                                                     <div className="pixel" style={{marginTop: "10px", width: "350px"}}>
@@ -2110,6 +2128,18 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                                                                     <div style={{margin: "0 5px"}}>750</div>
                                                                 </>
                                                             }
+                                                            {Number(item.Id) % 100000 === 18050 &&
+                                                                <>
+                                                                    <img src="../items/oversoul.png" height="18" alt="$OS"/>
+                                                                    <div style={{margin: "0 5px"}}>3850</div>
+                                                                    <i style={{fontSize: "12px", margin: "5px 10px 5px 5px"}} className="fa fa-plus"></i>
+                                                                    <img src="https://nftstorage.link/ipfs/bafkreia2bjrh7yw2vp23e5lnc6u75weg6nq7dzkyruggsnjxid6qtofeeq" height="18" alt="$JDAO"/>
+                                                                    <div style={{margin: "0 5px"}}>60</div>
+                                                                    <i style={{fontSize: "12px", margin: "5px 10px 5px 5px"}} className="fa fa-plus"></i>
+                                                                    <img src="https://nftstorage.link/ipfs/bafkreibizkouoitypq64ynygiclarbenejrtvsrfzeuezwh2b75fffyrzi" height="18" alt="$CMJ"/>
+                                                                    <div style={{margin: "0 5px"}}>800</div>
+                                                                </>
+                                                            }
                                                         </div>
                                                     </div>
                                                     {(Number(item.Id) % 100000 === 550 || Number(item.Id) % 100000 === 1450 || Number(item.Id) % 100000 === 2650 || Number(item.Id) % 100000 === 1050 || Number(item.Id) % 100000 === 2750 || Number(item.Id) % 100000 === 5250 || Number(item.Id) % 100000 === 8550 || Number(item.Id) % 100000 === 2550 || Number(item.Id) % 100000 === 4600 || Number(item.Id) % 100000 === 7500 || Number(item.Id) % 100000 === 11200) &&
@@ -2126,6 +2156,15 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                                                             <div className="emp">
                                                                 <i style={{fontSize: "18px", marginRight: "5px"}} className="fa fa-gavel"></i>
                                                                 Success rate : 1/3
+                                                            </div>
+                                                            <div>(depend on parent blockhash calculation)</div>
+                                                        </div>
+                                                    }
+                                                    {(Number(item.Id) % 100000 === 18050) &&
+                                                        <div style={{margin: "10px 0", width: "350px"}} className="pixel">
+                                                            <div className="emp">
+                                                                <i style={{fontSize: "18px", marginRight: "5px"}} className="fa fa-gavel"></i>
+                                                                Success rate : 1/4
                                                             </div>
                                                             <div>(depend on parent blockhash calculation)</div>
                                                         </div>
@@ -2184,6 +2223,8 @@ const Npcblacksmith = ({ setisLoading, txupdate, setTxupdate, enchantNABI, encha
                                                                 enchantRHandle3(item.Id, 116)
                                                             } else if (Number(item.Id) % 100000 === 13350) {
                                                                 enchantRHandle3(item.Id, 117)
+                                                            } else if (Number(item.Id) % 100000 === 18050) {
+                                                                enchantRHandle3(item.Id, 118)
                                                             }
                                                         }}
                                                     >
