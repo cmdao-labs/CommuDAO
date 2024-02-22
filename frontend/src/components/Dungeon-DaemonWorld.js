16c955286c21aeac6f649682a3fd83b3e4791eea
chattrapan <copasstragoon@gmail.com>
2024-02-22 11:44:40 +0700

    cmpow -> power

--- frontend/src/components/Dungeon-DaemonWorld.js
+++ frontend/src/components/Dungeon-DaemonWorld.js
@@ -724,7 +724,7 @@ const Daemonworld = ({ intrasubModetext, navigate, setisLoading, txupdate, setTx
                                     <img src={item.Image} height="100%" alt="Can not load metadata." />
                                 </div>
                                 <div className="emp bold">{item.Name}</div>
-                                <div className="bold">{item.RewardPerSec} cmpow per sec</div>
+                                <div className="bold">{item.RewardPerSec} power per sec</div>
                                 <div style={{fontSize: "12px", textAlign: "left", wordBreak: "break-word"}} className="light">{item.Description}</div>
                                 {address === youraddr ?
                                     <div style={{width: "80%", display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
