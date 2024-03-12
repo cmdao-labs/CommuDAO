bb8a3f43af5a586b4f302cc655133efa81820885
chattrapan <copasstragoon@gmail.com>
2024-03-12 07:18:43 +0700

    Spoiler

--- frontend/src/components/Mall.js
+++ frontend/src/components/Mall.js
@@ -83,6 +83,7 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
     const [sell26Remain, setSell26Remain] = React.useState(250)
     const [sell27Remain, setSell27Remain] = React.useState(250)
     const [sell28Remain, setSell28Remain] = React.useState(250)
+    const [sell29Remain, setSell29Remain] = React.useState(250)
 
     const [roll1Remain, setRoll1Remain] = React.useState(107)
     const [canroll1, setCanRoll1] = React.useState(false)
@@ -452,6 +453,12 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
                         functionName: 'sellList',
                         args: [12],
                     },
+                    {
+                        address: cmdaoMerchantV105,
+                        abi: cmdaoMerchantV2ABI,
+                        functionName: 'sellList',
+                        args: [13],
+                    },
                 ],
             })
             
@@ -483,6 +490,7 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
             const sell26Id = data2[31]
             const sell27Id = data2[32]
             const sell28Id = data2[33]
+            const sell29Id = data2[34]
             const roll1 = data2[6]
             const roll2 = data2[7]
             const roll3 = data2[8]
@@ -538,6 +546,7 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
             const sell26remain = (300025100000 - (Number(sell26Id.sellId) - 18800)) / 100000
             const sell27remain = (200025100000 - (Number(sell27Id.sellId) - 18800)) / 100000
             const sell28remain = (500025100000 - (Number(sell28Id.sellId) - 18800)) / 100000
+            const sell29remain = (400025100000 - (Number(sell29Id.sellId) - 18800)) / 100000
 
             const roll1remain = Number(roll1.nftCount)
             const roll2remain = Number(roll2.nftCount)
@@ -554,7 +563,7 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
                 _canBuy6, roll1remain, _canRoll1, roll2remain, roll3remain, roll4remain, roll5remain, _canRoll2, sell7remain, _canBuy7, 
                 sell8remain, _canBuy8, sell9remain, _canBuy9, sell10remain, _canBuy10, sell11remain, _canBuy11, sell12remain, _canBuy12, 
                 sell13remain, _canBuy13, roll6remain, sell14remain, _canBuy14, sell15remain, sell16remain, sell17remain, sell18remain, _canBuy18, 
-                sell19remain, sell20remain, sell21remain, sell22remain, sell23remain, _canBuy23, sell24remain, sell25remain, sell26remain, sell27remain, sell28remain,
+                sell19remain, sell20remain, sell21remain, sell22remain, sell23remain, _canBuy23, sell24remain, sell25remain, sell26remain, sell27remain, sell28remain, sell29remain,
                 ctunaBal, sx31Bal, jusdtBal, cmjBal, bbqBal, pzaBal, cuBal, jaspBal, osBal, goldBal, wjbcBal, swarBal, silBal, jdaoBal, angbBal, 
             ]
         }
@@ -613,6 +622,7 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
             setSell26Remain(result[49])
             setSell27Remain(result[50])
             setSell28Remain(result[51])
+            setSell29Remain(result[52])
 
             setRoll1Remain(result[12])
             setCanRoll1(result[13])
@@ -623,21 +633,21 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
             setCanRoll2(result[18])
             setRoll6Remain(result[33])
 
-            setCTunaBalance(ethers.utils.formatEther(String(result[52])))
-            setSx31Balance(ethers.utils.formatEther(String(result[53])))
-            setJusdtBalance(ethers.utils.formatEther(String(result[54])))
-            setCmjBalance(ethers.utils.formatEther(String(result[55])))
-            setBbqBalance(ethers.utils.formatEther(String(result[56])))
-            setPzaBalance(ethers.utils.formatEther(String(result[57])))
-            setCuBalance(ethers.utils.formatEther(String(result[58])))
-            setJaspBalance(ethers.utils.formatUnits(String(result[59]), "gwei"))
-            setOsBalance(ethers.utils.formatEther(String(result[60])))
-            setGoldBalance(ethers.utils.formatEther(String(result[61])))
-            setWjbcBalance(ethers.utils.formatEther(String(result[62])))
-            setSwarBalance(ethers.utils.formatEther(String(result[63])))
-            setSilBalance(ethers.utils.formatEther(String(result[64])))
-            setJdaoBalance(ethers.utils.formatEther(String(result[65])))
-            setAngbBalance(ethers.utils.formatEther(String(result[66])))
+            setCTunaBalance(ethers.utils.formatEther(String(result[53])))
+            setSx31Balance(ethers.utils.formatEther(String(result[54])))
+            setJusdtBalance(ethers.utils.formatEther(String(result[55])))
+            setCmjBalance(ethers.utils.formatEther(String(result[56])))
+            setBbqBalance(ethers.utils.formatEther(String(result[57])))
+            setPzaBalance(ethers.utils.formatEther(String(result[58])))
+            setCuBalance(ethers.utils.formatEther(String(result[59])))
+            setJaspBalance(ethers.utils.formatUnits(String(result[60]), "gwei"))
+            setOsBalance(ethers.utils.formatEther(String(result[61])))
+            setGoldBalance(ethers.utils.formatEther(String(result[62])))
+            setWjbcBalance(ethers.utils.formatEther(String(result[63])))
+            setSwarBalance(ethers.utils.formatEther(String(result[64])))
+            setSilBalance(ethers.utils.formatEther(String(result[65])))
+            setJdaoBalance(ethers.utils.formatEther(String(result[66])))
+            setAngbBalance(ethers.utils.formatEther(String(result[67])))
         })
 
     }, [address, txupdate, kycABI, ctunaLabABI, cmdaoMerchantABI, cmdaoMerchantV2ABI, cmdaoMerchantKYCABI, cmdaoGasha02ABI, erc20ABI])
@@ -2114,6 +2124,49 @@ const Mall = ({ setisLoading, txupdate, setTxupdate, kycABI, ctunaLabABI, cmdaoM
                                         }
                                     </> :
                                     <div style={{borderRadius: "12px", alignSelf: "flex-start", padding: "15px", fontSize: "16px", marginTop: "25px", width: "180px", display: "flex", justifyContent: "center", background: "#e9eaeb", color: "#bdc2c4", cursor: "not-allowed"}} className="pixel button">OUT OF STOCK</div>
+                                }
+                            </> :
+                            <div style={{borderRadius: "12px",alignSelf: "flex-start", padding: "15px", fontSize: "16px", marginTop: "25px", width: "180px", display: "flex", justifyContent: "center", background: "#e9eaeb", color: "#bdc2c4", cursor: "not-allowed"}} className="pixel button">Please connect wallet</div>
+                        }
+                    </div>
+
+                    <div className="nftCard" style={{position: "relative", justifyContent: "flex-start", height: "460px", margin: "20px", boxShadow: "6px 6px 0 #00000040", border: "1px solid rgb(227, 227, 227)"}}>
+                        <div style={{position: "absolute", top: -25, right: -15, padding: "5px 20px", width: "150px", background: "#fff", letterSpacing: 1, border: "1px solid rgb(227, 227, 227)", boxShadow: "6px 6px 0 #00000040", display: "flex", flexDirection: "row", alignItems: "center", zIndex: 1}} className="bold">
+                            <img src="https://nftstorage.link/ipfs/bafybeifetzaiv2i5anifvhumtbnq6ayvyqvrjn6hmq63vggg3r26gvzxce" width="30px" alt="TAODUM-TAOMEME" />
+                            <div className='light' style={{marginLeft: "10px"}}>TAODUM & TAOMEME</div>
+                        </div>
+                        <div style={{alignSelf: "flex-start", fontSize: "16px", width: "380px"}} className="pixel">NARUTA NFT - Phoenix Phalanx Ring (Accessory)</div>
+                        <img style={{alignSelf: "flex-start", marginTop: "20px"}} src="https://bafybeifmkrjl457rtfkve2ktn3ynx4yev5oqygpsvppeo4csm26fqormha.ipfs.nftstorage.link/" height="150" alt="PHOENIX-PHALANX-RING"/>
+                        <div style={{alignSelf: "flex-start", marginTop: "10px", minHeight: "200px", fontSize: "15px"}} className="pixel">
+                            <div style={{marginTop: "20px", width: "350px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", borderBottom: "1px solid #d9d8df"}}>
+                                <div>Limited</div>
+                                <div style={{display: "flex", flexDirection: "row"}}>
+                                    <div className="emp">{250}</div>
+                                    /250 EA
+                                </div>
+                            </div>
+                            <div style={{marginTop: "15px", width: "350px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", borderBottom: "1px solid #d9d8df"}}>
+                                <div>Status</div>
+                                <div style={{display: "flex", flexDirection: "row"}}>18800 Power</div>
+                            </div>
+                            <div style={{marginTop: "15px", width: "350px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", borderBottom: "1px solid #d9d8df"}}>
+                                <div>Price</div>
+                                <div style={{display: "flex", flexDirection: "row"}}>
+                                    <img src="https://nftstorage.link/ipfs/bafkreif3vllg6mwswlqypqgtsh7i7wwap7zgrkvtlhdjoc63zjm7uv6vvi" height="18" alt="jusdt"/>
+                                    <div style={{marginLeft: "7.5px"}}>20</div>
+                                </div>
+                            </div>
+                        </div>
+                        {address !== null && address !== undefined ?
+                            <>
+                                {false && sell29Remain > 0 ?
+                                    <>
+                                        {canbuy4 ?
+                                            <div style={{borderRadius: "12px", alignSelf: "flex-start", padding: "15px", fontSize: "16px", marginTop: "25px", width: "180px", display: "flex", justifyContent: "center"}} className="pixel button" onClick={() => buyHandle15(13)}>BUY</div> :
+                                            <div style={{borderRadius: "12px", alignSelf: "flex-start", padding: "15px", fontSize: "16px", marginTop: "25px", width: "180px", display: "flex", justifyContent: "center", background: "#e9eaeb", color: "#bdc2c4", cursor: "not-allowed"}} className="pixel button">INADEQUATE BALANCE</div>
+                                        }
+                                    </> :
+                                    <div style={{borderRadius: "12px", alignSelf: "flex-start", padding: "15px", fontSize: "16px", marginTop: "25px", width: "180px", display: "flex", justifyContent: "center", background: "#e9eaeb", color: "#bdc2c4", cursor: "not-allowed"}} className="pixel button">ON STOCK 10PM, 12.03</div>
                                 }
                             </> :
                             <div style={{borderRadius: "12px",alignSelf: "flex-start", padding: "15px", fontSize: "16px", marginTop: "25px", width: "180px", display: "flex", justifyContent: "center", background: "#e9eaeb", color: "#bdc2c4", cursor: "not-allowed"}} className="pixel button">Please connect wallet</div>
