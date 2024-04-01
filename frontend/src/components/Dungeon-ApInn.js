613668ce5975883e02842b0cf26517e4560c9323
coshi-labs <copasstragoon@gmail.com>
2024-04-01 13:04:05 +0700

    Ranker Link Fixed

--- frontend/src/components/Dungeon-ApInn.js
+++ frontend/src/components/Dungeon-ApInn.js
@@ -596,7 +596,7 @@ const ApInn = ({ setisLoading, txupdate, setTxupdate, acUpgradeABI, uniEnchanter
                                                 <div style={{width: "350px", marginRight: "50px", display: "flex", flexDirection: "row", justifyContent: "space-between", borderBottom: "1px dotted"}} key={index}>
                                                     <div style={{width: "200px", display: "flex", flexDirection: "row"}}>
                                                         <div>{index+1}</div>
-                                                        <a style={{textDecoration: "none", color: "#fff", marginLeft: "10px"}} href={"https://commudao.xyz/dungeon/daemon-world/" + item.addr} target="_blank" rel="noreferrer"><div className="bold">{item.name}</div></a>
+                                                        <a style={{textDecoration: "none", color: "#fff", marginLeft: "10px"}} href={"https://commudao.xyz/dungeon/daemon-world/" + item.from} target="_blank" rel="noreferrer"><div className="bold">{item.name}</div></a>
                                                     </div>
                                                     <div>{Number(item.value).toFixed(3)} $ANGB</div>
                                                 </div>
@@ -621,7 +621,7 @@ const ApInn = ({ setisLoading, txupdate, setTxupdate, acUpgradeABI, uniEnchanter
                                                 <div style={{width: "350px", marginRight: "50px", display: "flex", flexDirection: "row", justifyContent: "space-between", borderBottom: "1px dotted"}} key={index}>
                                                     <div style={{width: "200px", display: "flex", flexDirection: "row"}}>
                                                         <div>{index+1}</div>
-                                                        <a style={{textDecoration: "none", color: "#fff", marginLeft: "10px"}} href={"https://commudao.xyz/dungeon/daemon-world/" + item.addr} target="_blank" rel="noreferrer"><div className="bold">{item.name}</div></a>
+                                                        <a style={{textDecoration: "none", color: "#fff", marginLeft: "10px"}} href={"https://commudao.xyz/dungeon/daemon-world/" + item.from} target="_blank" rel="noreferrer"><div className="bold">{item.name}</div></a>
                                                     </div>
                                                     <div>{Number(item.value).toFixed(3)} $VABAG</div>
                                                 </div>
@@ -646,7 +646,7 @@ const ApInn = ({ setisLoading, txupdate, setTxupdate, acUpgradeABI, uniEnchanter
                                                 <div style={{width: "350px", marginRight: "50px", display: "flex", flexDirection: "row", justifyContent: "space-between", borderBottom: "1px dotted"}} key={index}>
                                                     <div style={{width: "200px", display: "flex", flexDirection: "row"}}>
                                                         <div>{index+1}</div>
-                                                        <a style={{textDecoration: "none", color: "#000", marginLeft: "10px"}} href={"https://commudao.xyz/dungeon/daemon-world/" + item.addr} target="_blank" rel="noreferrer"><div className="bold">{item.name}</div></a>
+                                                        <a style={{textDecoration: "none", color: "#000", marginLeft: "10px"}} href={"https://commudao.xyz/dungeon/daemon-world/" + item.from} target="_blank" rel="noreferrer"><div className="bold">{item.name}</div></a>
                                                     </div>
                                                     <div>{Number(item.value).toFixed(2)} $JUSDT</div>
                                                 </div>
