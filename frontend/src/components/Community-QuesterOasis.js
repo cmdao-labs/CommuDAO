1d4874bf43433937ce0933107c6b36157c8ab893
coshi-labs <copasstragoon@gmail.com>
2024-04-08 21:03:45 +0700

    Update APR 2024 Reward Prize

--- frontend/src/components/Community-QuesterOasis.js
+++ frontend/src/components/Community-QuesterOasis.js
@@ -529,19 +529,19 @@ const QuesterOasis = ({ setisLoading, txupdate, setTxupdate, erc20ABI, kycABI, q
                     <div style={{width: "98%", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between"}}>
                         <div style={{width: "220px", marginRight: "50px", display: "flex", flexDirection: "row", justifyContent: "space-between", borderBottom: "1px dotted"}}>
                             <div>Top Questers</div>
-                            <div>XXXX.00 CMJ</div>
+                            <div>7,674.50 CMJ</div>
                         </div>
                         <div style={{width: "220px", marginRight: "50px", display: "flex", flexDirection: "row", justifyContent: "space-between", borderBottom: "1px dotted"}}>
                             <div>Top Strongest</div>
-                            <div>XXXX.00 CMJ</div>
+                            <div>7,674.50 CMJ</div>
                         </div>
                         <div style={{width: "220px", marginRight: "50px", display: "flex", flexDirection: "row", justifyContent: "space-between", borderBottom: "1px dotted"}}>
                             <div>Top Spender</div>
-                            <div>XXXX.00 CMJ</div>
+                            <div>7,674.50 CMJ</div>
                         </div>
                         <div style={{width: "220px", marginRight: "50px", display: "flex", flexDirection: "row", justifyContent: "space-between", borderBottom: "1px dotted"}}>
                             <div>Top Money Mover</div>
-                            <div>XXXX.00 CMJ</div>
+                            <div>7,674.50 CMJ</div>
                         </div>
                     </div>
                     <div style={{color: "#fff"}}>Snapshot on the last block of the month before 0.00 AM.<br></br>Rewards will allocated to top 20 for each leaderboard.</div>
