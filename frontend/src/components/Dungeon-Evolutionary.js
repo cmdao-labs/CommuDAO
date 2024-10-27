d1fc183c00068a2491159e75a4d5ca0783aceaa0
coshi190 <copasstragoon@gmail.com>
2024-10-28 00:22:36 +0700

    feat: improve multichain ux

--- frontend/src/components/Dungeon-Evolutionary.js
+++ frontend/src/components/Dungeon-Evolutionary.js
@@ -934,8 +934,8 @@ const NpcEvolutionary = ({ config, navigate, callMode, setisLoading, txupdate, s
                     </div>
                 </div> :
                 <div style={{width: "90%", display: "flex", flexDirection: "row", justifyContent: "space-between", flexWrap: "wrap", overflow: "scroll"}} className="noscroll">
-                    <div style={{textAlign: "left", marginTop: "50px", display: "flex", flexDirection: "column", justifyContent: "flex-start"}}>
-                        <div style={{width: "250px", fontSize: "16px", letterSpacing: "1px"}} className="bold">Tokens</div>
+                    <div style={{maxWidth: "100%", textAlign: "left", marginTop: "50px", display: "flex", flexDirection: "column", justifyContent: "flex-start"}}>
+                        <div style={{width: "250px", ontSize: "16px", letterSpacing: "1px"}} className="bold">Tokens</div>
                         <div style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", overflow: "scroll"}} className="noscroll pixel">
                             <div style={{width: "200px", minWidth: "200px", height: "55px", margin: "20px 10px 20px 0", fontSize: "15px", border: "1px solid #dddade", boxShadow: "3px 3px 0 #dddade"}} className="items">
                                 <img src="https://apricot-secure-ferret-190.mypinata.cloud/ipfs/bafkreiabbtn5pc6di4nwfgpqkk3ss6njgzkt2evilc5i2r754pgiru5x4u" width="22" alt="$CMJ"/>
